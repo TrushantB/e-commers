344567419cd295e18556d78bb0e7ff5ed7767af8
TrushantB <trushant.ivision@gmail.com>
2020-10-21 19:01:10 +0530

    Updates

---
+++ precache-manifest.83eaa9eafa1483651da6aad85ead813d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db3d28fc7d12413a08a4a1cd0668d2f",
+    "url": "/e-commers/index.html"
+  },
+  {
+    "revision": "a5a4362f5101ffa85fd3",
+    "url": "/e-commers/static/css/2.ecac6ba9.chunk.css"
+  },
+  {
+    "revision": "6925002fc0697ed01e46",
+    "url": "/e-commers/static/css/main.6b1f9d86.chunk.css"
+  },
+  {
+    "revision": "a5a4362f5101ffa85fd3",
+    "url": "/e-commers/static/js/2.d23276ff.chunk.js"
+  },
+  {
+    "revision": "6925002fc0697ed01e46",
+    "url": "/e-commers/static/js/main.f059f7c6.chunk.js"
+  },
+  {
+    "revision": "3b95108b838ca0cce58d",
+    "url": "/e-commers/static/js/runtime~main.69d173ca.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/e-commers/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/e-commers/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/e-commers/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/e-commers/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/e-commers/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/e-commers/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/e-commers/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/e-commers/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/e-commers/static/media/slick.f97e3bbf.svg"
+  }
+]);
