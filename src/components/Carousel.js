154de7232dc72e432b254fe0a36aedb2c64d6a08
TrushantB <trushant.ivision@gmail.com>
2020-10-23 17:12:50 +0530

    text on slider changes

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -15,60 +15,59 @@ export default class Carousel extends Component {
       slidesToShow: 1,
       slidesToScroll: 1,
     };
-    const slide=['slide-one','slide-two','slide-three',"slide-four"]
     let data =[
       {
         id:1,
-        image:'https://via.placeholder.com/1300x500'
+        category:"flower",
+        title:"Thank You",
+        subtitle:"Greeting Cards",
+        label:"Loren ipsum",
+        image:'https://www.sagefruit.com/wp-content/uploads/2017/02/apricots-1509634_1920-2-1300x500.jpg'
       },
       {
-        id:1,
-        image:'https://images.unsplash.com/photo-1541507638585-94ea969f4608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+        id:2,
+        category:"flower",
+        title:"Thank You",
+        subtitle:"Greeting Cards",
+        label:"Loren ipsum",
+        image:'https://delmonte-sitefinity-public.s3.amazonaws.com/images/default-source/hompage-carousel-backgrounds/delmonte-homepagecarousel--pride_rainbow_horizontal_hi_branded_heart_june2019_global-1300x500.jpg?sfvrsn=3eec0d17_2'
       },
       {
-        id:1,
-        image:'https://images.unsplash.com/photo-1537617173434-be35dd906e61?ixlib=rb-1.2.1&auto=format&fit=crop&w=741&q=80'
+        id:3,
+        category:"flower",
+        title:"Thank You",
+        subtitle:"Greeting Cards",
+        label:"Loren ipsum",
+        image:'https://iamherbalifenutrition.com/wp-content/uploads/2019/07/Exotic-Fruits-1300x500.png'
       },
       {
-        id:1,
-        image:'https://images.unsplash.com/photo-1580926608062-464f40bdcec2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+        id:4,
+        category:"flower",
+        title:"Thank You",
+        subtitle:"Greeting Cards",
+        image:'https://www.nutritionforostomates.com.au/wp-content/uploads/2016/06/food-nutrients-for-ostomates-1300x500.jpg',
+        label:"Loren ipsum"
       }
     ]
     return (
-      <Slider className="row main-slider" {...settings}>
-        {  data.map((item,index) => {
-                return (
-            <div className="slick-wrapper" key={index}>
-                <div className={`slider `}>
-                  <img src={item.image} className={`${slide[index]}`} />
-                  
-                    {/* <div className="sliderMax">
-                        <h1 className="white">Organic Fresh tomato</h1>
-                        <h3 className="mb-4 white">tomato 1 kg</h3>
-                        <button className="btn">Read More</button>
-                    </div> */}
-                </div>
-            </div>
-          )
-          })
-  }  
+      <div className="slider-wrapper">
 
-        {/* <div>
-          <img width="100%" height="528px" src='https://images.unsplash.com/photo-1568045244282-e63a8b6b2b3d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'/>
-        </div>
-        <div >
-          <img width= "100%" height="528px" src='https://images.unsplash.com/photo-1541507638585-94ea969f4608?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'/>
-          </div>
-        <div>
-          <img width= "100%" height="528px" src='https://images.unsplash.com/photo-1537617173434-be35dd906e61?ixlib=rb-1.2.1&auto=format&fit=crop&w=741&q=80'/>
-          </div>
-        <div>
-          <img width="100%" height="528px" src='https://images.unsplash.com/photo-1580926608062-464f40bdcec2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'/>
-        </div>
-        <div>
-          <img width="100%" height="528px" src='https://images.unsplash.com/photo-1582273941194-b9512bd33030?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'/>
-        </div> */}
-      </Slider>
+        <Slider {...settings}>
+
+          {data.map((slide) =>
+
+            <div className="slick-slide" key={slide.id}>
+              <img className="slick-slide-image" src={`${slide.image}`} />
+              <label className="slick-slide-label">{slide.category}</label>
+              {/* <label className="slick-slide-label">{slide.title}</label> */}
+              {/* <label className="slick-slide-label">{slide.subtitle}</label> */}
+            </div>
+
+          )}
+
+        </Slider>
+
+      </div>
     );
   }
 };
