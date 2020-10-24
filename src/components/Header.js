04d4a44a2fb1a10835f265a9263681064b5ccba8
TrushantB <trushant.ivision@gmail.com>
2020-10-24 20:32:49 +0530

    adding menu drawer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,40 @@
 import React,{ Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import '../styles/Home/Header.css'
+import { slide as Menu } from 'react-burger-menu'
 
 class Header extends Component{
+    state = {
+        flag:false
+    }
+
+    handleSidenav() {
+        if(!this.state.flag) {
+            document.getElementById("mySidenav").style.width = "350px";
+        } else {
+            document.getElementById("mySidenav").style.width = "0px";
+
+        }
+        this.setState({flag:!this.state.flag})
+    }
 
     render(){
         return(
             <nav className='container'>
                 <div className='row py-4 align-items-center'>
                 <div>
-                    <i class="fa fa-bars" aria-hidden="true"></i>
+                    <i class="fa fa-bars" aria-hidden="true" onClick={() => this.handleSidenav()}></i>
                 </div>
+
+                    <div id="mySidenav" className="sidenav">
+                        <a href="javascript:void(0)" className="closebtn" onClick={() => this.handleSidenav()}>&times;</a>
+                        <Link to="/about" onClick = {() => this.handleSidenav()}>About</Link>
+                        <Link to="/product" onClick = {() => this.handleSidenav()}>Product</Link>
+                        <Link to="/Category" onClick = {() => this.handleSidenav()}>Category</Link>
+                        
+                    </div>
+
                     <Link to='/' className="navbar-brand rounded-0 ">
                         <img src= {require('../images/flipfairy-logo.png')}/> 
                     </Link>
